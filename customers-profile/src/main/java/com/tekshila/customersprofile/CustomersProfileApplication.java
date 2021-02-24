891f7f28217ec62f46c3034bba75ce28655e151e
tekshila <67528077+tekshila@users.noreply.github.com>
2021-02-24 15:33:56 -0500

    updated...and merged feature_user branch

---
+++ customers-profile/src/main/java/com/tekshila/customersprofile/CustomersProfileApplication.java
@@ -0,0 +1,13 @@
+package com.tekshila.customersprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersProfileApplication.class, args);
+    }
+
+}
